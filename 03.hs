1df28f93d226af341e788d99bb1204ca035b84d4
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-05-15 18:02:29 +0300

    Update 03.hs

--- 03.hs
+++ 03.hs
@@ -1,1 +1,8 @@
 --3. Реализовать на языке Haskell функцию, удаляющую из исходного списка элементы с четными номерами
+
+delEvenNumb :: [Int] -> [Int]
+delEvenNumb [] = []
+delEvenNumb (x:xs) = x : delEvenNumb (drop 1 xs)
+
+-- *Main> delEvenNumb [1, 2, 3, 4, 5]
+--[1, 3, 5]
